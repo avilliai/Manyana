267110e4f4f7ca1da71596cf1474bd021c371098
DEII <1840094972@qq.com>
2023-07-20 21:58:52 +0800

    memes

--- plugins/setuModerate.py
+++ plugins/setuModerate.py
@@ -10,6 +10,5 @@ async def setuModerate(img_url,moderateKey):
         print(r.json().get('predictions').get('adult'))
         return r.json().get('predictions').get('adult')
 if __name__ == '__main__':
-    a="http://gchat.qpic.cn/gchatpic_new/3552663628/608443475-2630406812-F801C42FB8A2E650EC2F313BFA70C3B5/0?term=2&is_origin=0"
-    b="207b10178c64089d90ebfcd7f865d97a"
+
     asyncio.run(setuModerate(a,b))
