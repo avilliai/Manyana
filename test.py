d6f6f1801b622667f1b29ce695823d16e19ce02d
DEII <1840094972@qq.com>
2023-07-22 08:39:49 +0800

    塔罗牌

--- test.py
+++ test.py
@@ -1,20 +1,45 @@
-import asyncio
+import datetime
+import os
 
-import httpx
+from vits import utils
 
-'''sasda = urllib.request.urlopen(
-                "https://api.moderatecontent.com/moderate/?url="+img_url+"&key="+moderateKey).read()
-            asf = json.loads(sasda.decode('UTF-8'))
-            rate = asf.get('predictions').get('adult')'''
+def modelLoader():
+    global modelDll
+    modelDll = {}
+    a = os.listdir('vits/voiceModel')
+    # print(type(a))
+    ind = 0
 
+    global CHOISE
+    CHOISE = {}
 
-async def weatherQuery1():
-    img_url="http://gchat.qpic.cn/gchatpic_new/1840094972/608443475-2712968684-6E5D2D3BCD5DF2EFD9EB80859BBFA8E2/0?term=2&is_origin=0"
-    moderateKey='207b10178c64089d90ebfcd7f865d97a'
-    url="https://api.moderatecontent.com/moderate/?url="+img_url+"&key="+moderateKey
-    async with httpx.AsyncClient(timeout=50) as client:
-        r = await client.get(url)
-        print(r.json().get('predictions').get('adult'))
-        return r.json().get('predictions').get('adult')
+    for i in a:
+        # print(i)
+
+        if os.path.isdir('vits/voiceModel/' + i):
+            # 内层循环遍历取出模型文件
+            file = os.listdir('vits/voiceModel/' + i)
+            for ass in file:
+                if ass.endswith('.pth'):
+                    hps_ms = utils.get_hparams_from_file('vits/voiceModel/' + i + '/config.json')
+                    speakers = hps_ms.speakers if 'speakers' in hps_ms.keys() else ['0']
+                    muspeakers = {}
+                    for id, name in enumerate(speakers):
+                        muspeakers[str(id)] = name
+                        CHOISE[name] = [str(id), ['voiceModel/' + i + '/' + ass, 'voiceModel/' + i + '/config.json']]
+
+                    modelDll[str(ind)] = ['voiceModel/' + i + '/' + ass, 'voiceModel/' + i + '/config.json', muspeakers]
+                    time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                    modelSelect = ['voiceModel/' + i + '/' + ass, 'voiceModel/' + i + '/config.json', muspeakers]
+
+                    print(time + '| 已读取' + 'voiceModel/' + i + '文件夹下的模型文件' + str(muspeakers))
+                    ind += 1
+            else:
+                pass
+        else:
+            pass
+    print(modelDll)
+    print(CHOISE)
+    return modelDll,modelSelect,CHOISE
 if __name__ == '__main__':
-    asyncio.run(weatherQuery1())
+    modelLoader()
