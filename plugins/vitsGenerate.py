04030755a12aff991ff20dc56605934279319aa9
DEII <1840094972@qq.com>
2023-07-22 12:08:50 +0800

    真不该大清早写代码

--- plugins/vitsGenerate.py
+++ plugins/vitsGenerate.py
@@ -8,7 +8,6 @@ from plugins.newLogger import newLogger
 
 
 async def voiceGenerate(data):
-    logger = newLogger()
     # 向本地 API 发送 POST 请求
     url = 'http://localhost:9080/synthesize'
     data = json.dumps(data)
@@ -16,8 +15,8 @@ async def voiceGenerate(data):
         async with httpx.AsyncClient(timeout=None) as client:
             await client.post(url, json=data)
     except:
-        logger.error("语音生成服务端意外关闭，执行重新唤醒")
+        print("语音生成服务端意外关闭，执行重新唤醒")
         subprocess.Popen(["python.exe", "flask_voice.py"], cwd="vits")
-        logger.info("服务端重新唤醒完成，重新执行语音生成")
+        print("服务端重新唤醒完成，重新执行语音生成")
         await voiceGenerate(data)
-    logger.info("语音生成完成")
+    print("语音生成完成")
