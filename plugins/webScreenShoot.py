353002594553c791521fe40e90f1e66352076413
DEII <1840094972@qq.com>
2023-07-29 19:18:27 +0800

    ba查询优化

--- plugins/webScreenShoot.py
+++ plugins/webScreenShoot.py
@@ -1,10 +1,6 @@
 from selenium import webdriver
 def webScreenShoot(url,path,width=1200,height=7500):
-
-    driverfile_path = r'plugins/msedgedriver.exe'
-
-
-    browser = webdriver.Edge(executable_path=driverfile_path)
+    browser = webdriver.Edge()
     url = url
     browser.set_window_size(width,height)
     browser.get(url)
