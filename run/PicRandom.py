2b144cde3129fff695031dc4f360fb0dba1aa0e5
DEII <1840094972@qq.com>
2023-07-30 19:34:59 +0800

    meme图升级

--- run/PicRandom.py
+++ run/PicRandom.py
@@ -6,13 +6,16 @@ import random
 import re
 import urllib
 from asyncio import sleep
+from io import BytesIO
 
+import httpx
 import yaml
 from mirai import Image, Voice, Startup
 from mirai import Mirai, WebSocketAdapter, FriendMessage, GroupMessage, At, Plain
 
+from plugins.RandomStr import random_str
 from plugins.tarot import tarotChoice
-
+from PIL import Image as Image1
 
 def main(bot,logger):
     logger.info("抽卡/运势模块启动")
@@ -20,10 +23,24 @@ def main(bot,logger):
     async def meme(event: GroupMessage):
         global memeData
         if str(event.message_chain) == "meme" or ("meme" in str(event.message_chain) and At(bot.qq) in event.message_chain):
-            la = os.listdir("data/pictures/meme")
-            la = "data/pictures/meme/" + random.choice(la)
-            logger.info("掉落了一张meme图")
-            await bot.send(event, (str(event.sender.member_name) + "得到了一张meme图", Image(path=la)))
+            try:
+                logger.info("使用网络meme")
+                headers = {
+                    'User-Agent': 'Apifox/1.0.0 (https://apifox.com)'
+                }
+                url = "https://oss.grass.starxw.com/service/image?type=download"
+                async with httpx.AsyncClient(timeout=20) as client:
+                    r = await client.get(url, headers=headers)
+                    img = Image1.open(BytesIO(r.content))  # 从二进制数据创建图片对象
+                    path="data/pictures/meme/"+random_str()+".png"
+                    img.save(path)  # 使用PIL库保存图片
+                    await bot.send(event,Image(path=path))
+            except:
+                logger.warning("网络meme出错，使用本地meme图")
+                la = os.listdir("data/pictures/meme")
+                la = "data/pictures/meme/" + random.choice(la)
+                logger.info("掉落了一张meme图")
+                await bot.send(event, (str(event.sender.member_name) + "得到了一张meme图", Image(path=la)))
 
     @bot.on(GroupMessage)
     async def meme(event: GroupMessage):
