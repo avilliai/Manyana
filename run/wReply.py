dd0a274dc81e9a8505e8db6c69e84dfa985d38c0
DEII <1840094972@qq.com>
2023-08-13 22:18:01 +0800

    优化配置文件

--- run/wReply.py
+++ run/wReply.py
@@ -224,7 +224,7 @@ def main(bot,config,sizhiKey,app_id, app_key,logger):
         else:
             #优先匹配本词库
             if str(event.group.id) in superDict.keys():
-                if random.randint(0,100)<groupLexicon:
+                if random.randint(0,100)<groupLexicon or At(bot.qq) in event.message_chain:
                     keys1=superDict.get(str(event.group.id)).keys()
                     lock=0
                     for i in keys1:
