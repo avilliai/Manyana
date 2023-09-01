ec1df6a6200a22100846e988e75ef76cf8cbab5b
DEII <1840094972@qq.com>
2023-09-01 23:17:34 +0800

    修复问题

--- run/aiReply.py
+++ run/aiReply.py
@@ -81,9 +81,14 @@ def main(bot,master,apikey,proxy,logger):
                             conversation_id=None
                             parent_message_id="f0bf0ebe-1cd6-4067-9264-8a40af76d00e"
                         else:
-                            pub=random.choice(pandoraData.keys())
-                            conversation_id = pandoraData.get(pub).get("conversation_id")
-                            parent_message_id = "f0bf0ebe-1cd6-4067-9264-8a40af76d00e"
+                            try:
+                                pub=random.choice(pandoraData.keys())
+                                conversation_id = pandoraData.get(pub).get("conversation_id")
+                                parent_message_id = pandoraData.get(pub).get("parent_message_id")
+                            except:
+                                await bot.send(event, "当前服务器负载过大，请稍后再试", True)
+                                totallink = False
+                                return
                     try:
                         parent_message_id, conversation_id,response_message = ask_chatgpt(prompt, model, message_id, parent_message_id,
                                                                          conversation_id)
