39c48df3b2ca81c7f376686af5e583683ad8d2a7
DEII <1840094972@qq.com>
2023-09-13 10:43:58 +0800

    制作一个chatLearning可用的配置文件

--- setUp.py
+++ setUp.py
@@ -4,7 +4,7 @@ from plugins.newLogger import newLogger
 
 
 def main():
-    print("请输入要执行的指令：\n1 绑定到远程仓库(如果通过源码包安装请执行)\n2 更新bot代码\n3 启动数据清理大师(doge)")
+    print("请输入要执行的指令：\n1 绑定到远程仓库(如果通过源码包安装请执行)\n2 更新bot代码\n3 启动数据清理大师(doge)\n4 导出群信息，制作一个chatLearning可用的配置文件")
 
     a=input("输入要执行的数字")
     if a=="1":
@@ -62,6 +62,78 @@ def main():
                 continue
         print("清理本地图库缓存完成,缓存的涩图都没喽")
         print("清理缓存完成")
+    elif a=="4":
+        import json
+        print("chatLearning： https://mirai.mamoe.net/topic/1018/chatlearning-%E8%AE%A9bot%E5%AD%A6%E4%BC%9A%E4%BD%A0%E7%9A%84%E7%BE%A4%E8%81%8A/8")
+        if os.path.exists("config.clc"):
+            with open("config.clc", "r", encoding="utf-8") as ass:
+                p = json.loads(ass.read())
+        else:
+            p = {
+                "unmergegrouplist": [],
+                "learninggrouplist": [
+                    628763673,
+                    699455559
+                ],
+                "replygrouplist": [
+                    628763673,
+                    699455559
+                ],
+                "interval": 900,
+                "sendmode": 0,
+                "mergetime": 900,
+                "learning": 0,
+                "merge": 0,
+                "reply": 0,
+                "replychance": 50,
+                "admin": 0,
+                "Administrator": [
+                    123456
+                ],
+                "blackfreq": 5,
+                "voicereply": 0,
+                "voicereplychance": 20,
+                "synthesizer": "",
+                "version": "3.0.3",
+                "tag": {},
+                "singlereplychance": {},
+                "singlevoicereplychance": {},
+                "typefreq": {},
+                "tempmessagenum": 32,
+                "fastdelete": 0,
+                "replywait": [
+                    0,
+                    0
+                ],
+                "voicecharerror": "存在违规字符，转换失败",
+                "voicecderror": "转换冷却中",
+                "voicelengtherror": "长度超过限制",
+                "deletesuccess": "已从词库内删除！",
+                "deletetemperror": "删除失败，该消息已不在缓存内",
+                "deletefinderror": "删除失败，词库中已无法找到该答案",
+                "replycd": 3,
+                "stopsign": 0,
+                "voicecommand": "快说 ",
+                "cosmatch": 0,
+                "cosmatching": 0.5,
+                "botname": [
+                    "我"
+                ],
+                "replylength": 100,
+                "atreply": 1
+            }
+        file1 = open('data/music/groups.txt', 'r')
+        js1 = file1.read()
+        js1 = json.loads(js1)
+        file1.close()
+        da = js1.keys()
+        print(list(da), type(list(da)))
+        p["replygrouplist"] = list(da)
+        p["learninggrouplist"] = list(da)
+        ja = json.dumps(p)
+        with open("config.clc", "w", encoding="utf-8") as fp:
+            fp.write(ja)
+        print("完毕，请用config.clc覆盖chatLearning文件夹下同名文件")
     else:
         print("结束")
 
