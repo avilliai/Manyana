261182a7e7d6868cf93dabb1519ee8087318d995
DEII <1840094972@qq.com>
2023-09-26 23:21:36 +0800

    菜单第三页

--- main.py
+++ main.py
@@ -107,6 +107,7 @@ if __name__ == '__main__':
             logger.info("获取菜单")
             await bot.send(event, Image(path='config/help1.png'))
             await bot.send(event, Image(path='config/help2.png'))
+            await bot.send(event, Image(path='config/help3.png'))
             await bot.send(event, '这是' + botName + '的功能列表\nヾ(≧▽≦*)o')
         if '制图' in str(event.message_chain) and At(bot.qq) in event.message_chain :
             logger.info("菜单")
