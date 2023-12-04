6ac331e07ebf80175389b6a46cc78f65cf86b737
DEII <1840094972@qq.com>
2023-12-04 21:27:25 +0800

    词库编辑权限限制

--- run/wReply.py
+++ run/wReply.py
@@ -177,11 +177,12 @@ def main(bot,config,sizhiKey,app_id, app_key,logger):
 
     @bot.on(GroupMessage)
     async def handle_group_message(event: GroupMessage):
-        global superDict
+        global superDict,userdict
         if str(event.message_chain) == '开始添加' or str(event.message_chain) == '*开始添加':
             #str(event.sender.id) in trustUser or
             if editPermission!=0 and event.sender.id!=master:
-                if str(event.sender.id) not in superDict or int(userdict.get(str(event.sender.id)).get("sts"))<editPermission:
+                if str(event.sender.id) not in userdict or int(userdict.get(str(event.sender.id)).get("sts"))<editPermission:
+                    logger.info("当前用户签到天数："+userdict.get(str(event.sender.id)).get("sts"))
                     await bot.send(event, "签到天数不足，请发送 签到 以完善用户信息，签到天数达到"+str(editPermission)+"后将开放词库权限")
                     return
 
