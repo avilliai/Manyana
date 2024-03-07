4161e2db953a5429c3bfe102df6d1c598f15b9df
DEII <1840094972@qq.com>
2024-03-08 00:05:20 +0800

    ai绘画,以图生图

--- plugins/aiDrawer.py
+++ plugins/aiDrawer.py
@@ -19,7 +19,14 @@ async def draw(prompt,path= "./test.png"):
         img.save(path)  # 使用PIL库保存图片
         # print(path)
         return path
-
+async def airedraw(prompt,url,path="./redraw.png"):
+    url=f"https://api.lolimi.cn/API/AI/isd.php?msg={prompt}&img={url}&mode=动漫"
+    async with httpx.AsyncClient(timeout=40) as client:
+        r = await client.get(url)
+        img = Image.open(BytesIO(r.content))  # 从二进制数据创建图片对象
+        img.save(path)  # 使用PIL库保存图片
+        # print(path)
+        return path
 # 运行 Flask 应用
 if __name__ == "__main__":
     asyncio.run(draw("正在吃早饭的二次元少女"))
--- run/aiDraw.py
+++ run/aiDraw.py
@@ -16,11 +16,13 @@ from mirai import Image, Voice
 from mirai import Mirai, WebSocketAdapter, FriendMessage, GroupMessage, At, Plain
 
 from plugins.RandomStr import random_str
-from plugins.aiDrawer import draw
+from plugins.aiDrawer import draw, airedraw
 
 
 def main(bot,logger):
     logger.info("ai绘画 启用")
+    global redraw
+    redraw={}
     @bot.on(GroupMessage)
     async def aidrawf(event: GroupMessage):
         if str(event.message_chain).startswith("画 "):
@@ -30,3 +32,24 @@ def main(bot,logger):
             p=await draw(tag,path)
             await bot.send(event,Image(path=p),True)
 
+    @bot.on(GroupMessage)
+    async def rededd(event: GroupMessage):
+        global redraw
+        if str(event.message_chain)=="以图生图 ":
+            await bot.send(event,"请发送图片(最近由于未知原因，有概率获取url失败)")
+            redraw[event.sender.id]=str(event.message_chain).replace("以图生图 ","")
+    @bot.on(GroupMessage)
+    async def redrawStart(event: GroupMessage):
+        global redraw
+        if event.message_chain.count(Image) and event.sender.id in redraw:
+            prompt=redraw.get(event.message_chain)
+            lst_img = event.message_chain.get(Image)
+            url1 = lst_img[0].url
+            try:
+                p=await airedraw(prompt,url1)
+                await bot.send(event,Image(path=p))
+            except Exception as e:
+                logger.error(e)
+                logger.error("ai绘画出错")
+                await bot.send(event,"ai绘画出错")
+                redraw.pop(event.sender.id)
