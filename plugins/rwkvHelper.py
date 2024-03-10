cb852ebb970d641116a1395343a0af04dd82b682
DEII <1840094972@qq.com>
2024-03-10 22:34:53 +0800

    语音合成新增模式

--- plugins/rwkvHelper.py
+++ plugins/rwkvHelper.py
@@ -10,18 +10,8 @@ async def rwkvHelper(text):
     async with httpx.AsyncClient(timeout=100) as client:
         data = {"messages": [{"role": "user","content": text}],"model": "rwkv","stream": False,"max_tokens": 1000,"temperature": 1.2,"top_p": 0.5,"presence_penalty": 0.4,"frequency_penalty": 0.4}
         r = await client.post(url,json=data)
-        #print(r.json())
-        #print(r.json().get('choices')[0].get("message").get("content"))
-
         return r.json().get('choices')[0].get("message").get("content")
 
-'''async def onlineRWKV(text):
-    client = Client(src="https://3aabd0bfae0d9019cf.gradio.live/",proxies={"http": "http://127.0.0.1:1080"})
-    result = client.predict(
-                    'null',	# str (Option from: []) in '请选择角色' Dropdown component
-                    fn_index=2
-    )
-    print(result)'''
 
 
 if __name__ == '__main__':
