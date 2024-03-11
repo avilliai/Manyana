4111a7d73178d7957f8b7a0e70d311e6b3d7d07a
avillia <99066610+avilliai@users.noreply.github.com>
2024-03-11 10:52:30 +0800

    更新 bertVits2.py

--- run/bertVits2.py
+++ run/bertVits2.py
@@ -28,7 +28,7 @@ def main(bot,logger,berturl,proxy):
     spe=result.get("bert_speakers")
     modelScope=["塔菲","阿梓","otto","丁真","星瞳","东雪莲","嘉然","孙笑川","亚托克斯","文静","鹿鸣"]
     @bot.on(GroupMessage)
-    async def taffySay(event:GroupMessage):
+    async def taffySayf(event:GroupMessage):
 
         if "说" in str(event.message_chain) and str(event.message_chain).split("说")[0] in spe:
             data={}
@@ -40,6 +40,9 @@ def main(bot,logger,berturl,proxy):
                 await bot.send(event,Voice(path=path))
             except:
                 pass
+
+    @bot.on(GroupMessage)
+    async def taffySays(event:GroupMessage)
         if "说" in str(event.message_chain) and str(event.message_chain).split("说")[0] in modelScope:
             try:
                 data = {}
@@ -48,5 +51,5 @@ def main(bot,logger,berturl,proxy):
                 logger.info("modelscopeTTS语音合成:"+data["text"])
                 path=await superVG(data,"modelscopeTTS")
                 await bot.send(event,Voice(path=path))
-            except:
-                pass
+            except Exception as e:
+                logger.error(e)
