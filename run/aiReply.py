2f2e3b5c167613f34c8ffc2d19ea0ddcc5a10403
avillia <99066610+avilliai@users.noreply.github.com>
2024-03-16 13:56:02 +0800

    Update aiReply.py

--- run/aiReply.py
+++ run/aiReply.py
@@ -1317,7 +1317,7 @@ def main(bot, master, logger):
             # 超过10，移除第一个元素
 
             if len(prompt1) > maxPrompt:
-                logger.error("gpt3.5 prompt超限，移除元素")
+                logger.error(f"{modelHere} prompt超限，移除元素")
                 del prompt1[0]
                 del prompt1[0]
             chatGLMData[event.sender.id] = prompt1
