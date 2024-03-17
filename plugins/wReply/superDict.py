548fea4d2bd22a7511adaa363a31e6e4d9803ff1
avillia <99066610+avilliai@users.noreply.github.com>
2024-03-17 16:37:33 +0800

    Update superDict.py

--- plugins/wReply/superDict.py
+++ plugins/wReply/superDict.py
@@ -15,55 +15,60 @@ def importDict(xlsxPath = 'data/autoReply/lexicon/public.xlsx'):
     ls1=os.listdir("data/autoReply/lexicon")
     diss={}
     for isa in ls1:
-        if isa.endswith(".xlsx"):
-            xlsxPath="data/autoReply/lexicon/"+isa
-            # 第一步打开工作簿
-            wb = openpyxl.load_workbook(xlsxPath)
-            # 第二步选取表单
-            sheet = wb.active
-            # 按行获取数据转换成列表
-            rows_data = list(sheet.rows)
-            # 获取表单的表头信息(第一行)，也就是列表的第一个元素
-            titles = [title.value for title in rows_data.pop(0)]
-            #print(titles)
 
-
-            all_row_dict = []
-
-            #当你需要向已有词库导入时取消注释
-            '''fileaa = open('Config\\superDict.txt', 'r')
-            js1 = fileaa.read()
-            newDict = json.loads(js1)
-            print('已读取现存字典')'''
-
-            #新建词库，当你需要新建词库时取消注释
-            newDict={}
-
-            # 遍历出除了第一行的其他行
-            for a_row in rows_data:
-                the_row_data = [cell.value for cell in a_row]
-                # 将表头和该条数据内容，打包成一个字典
-                row_dict = dict(zip(titles, the_row_data))
-                #print(row_dict)
-                all_row_dict.append(row_dict)
-            for i in all_row_dict:
-                key=i.get('key')
-                value=i.get('value')
-
-                if (key in newDict):
-                    replyValue=newDict.get(key)
-                    if value in replyValue:
-                        pass
-                        #print('已存在该回复，不添加')
+        try:
+            if isa.endswith(".xlsx"):
+                xlsxPath="data/autoReply/lexicon/"+isa
+                # 第一步打开工作簿
+                wb = openpyxl.load_workbook(xlsxPath)
+                # 第二步选取表单
+                sheet = wb.active
+                # 按行获取数据转换成列表
+                rows_data = list(sheet.rows)
+                # 获取表单的表头信息(第一行)，也就是列表的第一个元素
+                titles = [title.value for title in rows_data.pop(0)]
+                #print(titles)
+    
+    
+                all_row_dict = []
+    
+                #当你需要向已有词库导入时取消注释
+                '''fileaa = open('Config\\superDict.txt', 'r')
+                js1 = fileaa.read()
+                newDict = json.loads(js1)
+                print('已读取现存字典')'''
+    
+                #新建词库，当你需要新建词库时取消注释
+                newDict={}
+    
+                # 遍历出除了第一行的其他行
+                for a_row in rows_data:
+                    the_row_data = [cell.value for cell in a_row]
+                    # 将表头和该条数据内容，打包成一个字典
+                    row_dict = dict(zip(titles, the_row_data))
+                    #print(row_dict)
+                    all_row_dict.append(row_dict)
+                for i in all_row_dict:
+                    key=i.get('key')
+                    value=i.get('value')
+    
+                    if (key in newDict):
+                        replyValue=newDict.get(key)
+                        if value in replyValue:
+                            pass
+                            #print('已存在该回复，不添加')
+                        else:
+                            replyValue.append(value)
+                            #print('已有关键字，追加')
+                        #print(replyValue)
+                    # 没有关键字则创建
                     else:
-                        replyValue.append(value)
-                        #print('已有关键字，追加')
-                    #print(replyValue)
-                # 没有关键字则创建
-                else:
-                    newDict[key] = [value,]
-                #print('key:'+key+' '+'value:'+value)
-            diss[isa.split(".")[0]]=newDict
+                        newDict[key] = [value,]
+                    #print('key:'+key+' '+'value:'+value)
+                diss[isa.split(".")[0]]=newDict
+
+        except:
+            print("导入"+str(isa)+"异常")
             #print(isa+"导入完成")
     js = json.dumps(diss)
 
