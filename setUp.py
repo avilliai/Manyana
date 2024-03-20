0f3cd316bc099f8e9d370d7cbb2382d5cbcd5b3f
DEII <1840094972@qq.com>
2024-03-20 22:39:16 +0800

    语音合成speaker提示

--- setUp.py
+++ setUp.py
@@ -194,7 +194,7 @@ def updaat(f=False,jump=False,source="3"):
             # os.system("pip install selenium")
     if source=="3":
         logger.info("拉取bot代码\n--------------------")
-        logger.warning("建议优先选用与搭建源匹配的更新源")
+        logger.warning("建议优先选用与搭建源匹配的更新源，以免由于更新版本不一致导致意外状况")
         logger.info("选择更新源()：\n1 git源\n2 镜像源(无需代理，同步更新。兼容git源搭建的Manyana git源不可用时可选此项)")
         source = input("选择更新源(输入数字 )：")
     else:
