70141f2346e7bc20597c7a98510b7038a347b3ea
DEII <1840094972@qq.com>
2024-05-01 13:14:31 +0800

    fix bug

--- run/aiDraw.py
+++ run/aiDraw.py
@@ -83,7 +83,7 @@ def main(bot,logger):
                 except Exception as e:
                     logger.error(e)
                     logger.error("接口3绘画失败.......")
-                    return
+                    break
                     # await bot.send(event,"接口2绘画失败.......")
                 i += 1
             if i > 4:
