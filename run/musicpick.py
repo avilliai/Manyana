192281366dac69a804c49af30aaab69bd0600504
DEII <1840094972@qq.com>
2024-05-01 14:36:39 +0800

    电梓播放器，启动！

--- run/musicpick.py
+++ run/musicpick.py
@@ -12,6 +12,7 @@ import httpx
 import requests
 import utils
 import yaml
+from fuzzywuzzy import process
 from mirai import Image, Voice
 from mirai import Mirai, WebSocketAdapter, FriendMessage, GroupMessage, At, Plain
 from mirai.models import MusicShare
@@ -26,6 +27,22 @@ def main(bot,logger):
     with open('config/settings.yaml', 'r', encoding='utf-8') as f:
         resulta = yaml.load(f.read(), Loader=yaml.FullLoader)
     musicToVoice=resulta.get("musicToVoice")
+    @bot.on(GroupMessage)
+    async def dianzibofangqi(event: GroupMessage):
+        if str(event.message_chain)=="开溜" or (At(bot.qq) in event.message_chain and "唱歌" in str(event.message_chain)):
+            logger.info("电梓播放器，启动！")
+            p=os.listdir("data/music/audio")
+            await bot.send(event,Voice(path="data/music/audio/"+random.choice(p)))
+
+    @bot.on(GroupMessage)
+    async def dianzibofangqidiange(event: GroupMessage):
+        if str(event.message_chain).startswith("溜 ") or str(event.message_chain).startswith("唱 "):
+            logger.info("电梓播放器，启动！")
+            p = os.listdir("data/music/audio")
+            ha=process.extractBests(str(event.message_chain).replace("溜 ","").replace("唱 ",""), p, limit=3)
+            logger.info(ha[0][0])
+            p="data/music/audio/" + ha[0][0]
+            await bot.send(event, Voice(path=p))
     @bot.on(GroupMessage)
     async def selectMusic(event: GroupMessage):
         global musicTask
