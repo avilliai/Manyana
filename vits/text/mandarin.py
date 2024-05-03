bdb6c9a76e6b2ce484cafa5f49786367df21b283
DEII <1840094972@qq.com>
2024-05-04 01:01:49 +0800

    大道至简

--- vits/text/mandarin.py
+++ vits/text/mandarin.py
@@ -7,7 +7,7 @@ import cn2an
 import logging
 
 logging.getLogger('jieba').setLevel(logging.WARNING)
-jieba.set_dictionary('/vits/jieba/dict.txt')
+jieba.set_dictionary('./vits/jieba/dict.txt')
 jieba.initialize()
 
 
