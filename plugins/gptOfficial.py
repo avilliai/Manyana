b89b2bb6ea827122655602cf088405f13a1f4f6d
DEII <1840094972@qq.com>
2024-05-04 00:27:31 +0800

    模型增加random模式

--- plugins/gptOfficial.py
+++ plugins/gptOfficial.py
@@ -84,7 +84,6 @@ def qwen(prompt,bot_info):
     url = f"https://api.alcex.cn/API/ai/qwen.php?messages={prompt}"
     #print(url)
     r=requests.get(url).json()
-    print(r)
     return {"role": "assistant", "content": r["choices"][0]["message"]["content"]}
 def gptvvvv(prompt,bot_info):
     prompt.insert(0, {"role": "user", "content": bot_info})
@@ -94,7 +93,12 @@ def gptvvvv(prompt,bot_info):
     url = f"https://api.alcex.cn/API/gpt-4/v2.php?messages={prompt}&model=gpt-3.5-turbo"
     #print(url)
     r=requests.get(url).json()
-    print(r)
     return {"role": "assistant", "content": r["choices"][0]["message"]["content"]}
-
+def grop(prompt,bot_info):
+    prompt.insert(0, {"role": "user", "content": bot_info})
+    prompt.insert(1, {"role": "assistant", "content": "好的，已了解您的需求，我会根据您的需求扮演好您设定的角色。"})
+    prompt = str(prompt).replace("\"", "%22").replace("\'", "%22")
+    url=f"https://api.alcex.cn/API/ai/grop.php?messages={prompt}"
+    r=requests.get(url).json()
+    return {"role": "assistant", "content": r["choices"][0]["message"]["content"]}
 
