f9dc58be016c83a3cc7688b0fa45b42760760926
avillia <99066610+avilliai@users.noreply.github.com>
2024-05-09 02:32:07 +0800

    Update aiDraw.py

--- run/aiDraw.py
+++ run/aiDraw.py
@@ -6,17 +6,12 @@ from mirai import GroupMessage
 
 from plugins.RandomStr import random_str
 from plugins.aiDrawer import draw, airedraw, draw1, draw3,tiktokredraw
-from plugins.bingImageCreater.bingDraw import bingCreate
 
 
 def main(bot,logger):
     logger.info("ai绘画 启用")
     global redraw
     redraw={}
-    with open('config/api.yaml', 'r', encoding='utf-8') as f:
-        resulttr = yaml.load(f.read(), Loader=yaml.FullLoader)
-    sock5proxy=resulttr.get("sock5-proxy")
-    bing_image_creator_key=resulttr.get("bing-image-creator")
     @bot.on(GroupMessage)
     async def aidrawf(event: GroupMessage):
         if str(event.message_chain).startswith("画 "):
@@ -53,18 +48,6 @@ def main(bot,logger):
                 i+=1
             if i>7:
                 await bot.send(event, "接口绘画失败.......")
-    @bot.on(GroupMessage)
-    async def selfBingDraw(event: GroupMessage):
-        if str(event.message_chain).startswith("画"):
-            tag = str(event.message_chain).replace("画 ", "")
-            if bing_image_creator_key.get("_U")!="" and bing_image_creator_key.get("KievRPSSecAuth")!="":
-                try:
-                    logger.info(f"bing接口发起请求:{tag}")
-                    p=await bingCreate(sock5proxy,tag,bing_image_creator_key.get("_U"),bing_image_creator_key.get("KievRPSSecAuth"))
-                    await bot.send(event, [Image(path=p[0]), Image(path=p[1]), Image(path=p[2]), Image(path=p[3])], True)
-                except Exception as e:
-                    logger.error(e)
-                    await bot.send(event,"bing cookie过期，请检查")
     @bot.on(GroupMessage)
     async def aidrawff(event: GroupMessage):
         if str(event.message_chain).startswith("画 "):
