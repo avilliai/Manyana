5759004fdb0a62e50273c046412cdfae78ead536
avillia <99066610+avilliai@users.noreply.github.com>
2024-05-14 13:54:23 +0800

    Update setuModerate.py

--- plugins/setuModerate.py
+++ plugins/setuModerate.py
@@ -9,6 +9,16 @@ async def setuModerate(img_url,moderateKey):
         r = await client.get(url)
         print(r.json().get('predictions').get('adult'))
         return r.json().get('predictions').get('adult')
+async def fileImgModerate(filePath,moderateKey):
+    data = {
+        "file": (filePath, open(image_path, "rb")),
+        "key": moderateKey,
+    }
+    async with httpx.AsyncClient() as client:
+        r = await client.post(url="https://api.moderatecontent.com/moderate/", data=data)
+    print(r.json().get('predictions').get('adult'))
+    return r.json().get('predictions').get('adult')
+
 if __name__ == '__main__':
 
     asyncio.run(setuModerate(a,b))
