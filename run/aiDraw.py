947c14cccb2a616132f8a22e19c759646c6be316
avillia <99066610+avilliai@users.noreply.github.com>
2024-05-20 19:00:46 +0800

    Update aiDraw.py

--- run/aiDraw.py
+++ run/aiDraw.py
@@ -156,7 +156,8 @@ def main(bot,logger):
         if str(event.message_chain).startswith("画 ") and aiDrawController.get("接口6"):
             tag = str(event.message_chain).replace("画 ", "")
             path = "data/pictures/cache/" + random_str() + ".png"
-            if 1:
+            i=0
+            while i<5:
                 try:
                     logger.info("接口6绘画中......")
                     p = await draw6(tag, path)
@@ -175,9 +176,11 @@ def main(bot,logger):
                             logger.error("无法进行自我审核，错误的网络环境或apikey")
                             return
                     await bot.send(event, Image(path=p), True)
+                    return
                 except Exception as e:
                     logger.error(e)
                     logger.error("接口6绘画失败.......")
+                    i+=1
     @bot.on(GroupMessage)
     async def aidrawff5(event: GroupMessage):
         if str(event.message_chain).startswith("画 ") and aiDrawController.get("接口4"):
