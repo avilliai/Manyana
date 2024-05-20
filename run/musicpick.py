2b1d4668999eac2673bb58c54b501bdf68d537f0
avillia <99066610+avilliai@users.noreply.github.com>
2024-05-20 19:30:04 +0800

    Update musicpick.py

--- run/musicpick.py
+++ run/musicpick.py
@@ -58,8 +58,10 @@ def main(bot,logger):
                         await bot.send(event,"连接出错，或无对应歌曲")
                     else:
                         musicTask[event.sender.id]=musicName
-                        ffs=(r"', "+str(ffs)).replace(r"\n","\n").replace("[","").replace("]","").replace(r"', '","")
-                        await bot.send(event,f"请发送对应歌曲的序号:\n{ffs}",True)
+                        musicL=""
+                        for ib in ffs:
+                            musicL+=ib+"\n"
+                        await bot.send(event,f"请发送对应歌曲的序号:\n{musicL}",True)
                 else:
                     ffs = await cccdddm(musicName)
                     if ffs == None:
