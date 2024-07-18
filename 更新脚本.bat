aa3ae180af859f90d297785ae6a6385ef88a8c3b
avillia <99066610+avilliai@users.noreply.github.com>
2024-07-18 22:22:18 +0800

    Update 更新脚本.bat 兼容宝宝巴士启动器

--- 更新脚本.bat
+++ 更新脚本.bat
@@ -1,5 +1,14 @@
-cd venv/Scripts
-call activate.bat
-cd ../..
-python setUp.py
+@echo off
+set PYTHON_EXE=..\environments\Python39\python.exe
+set MAIN_SCRIPT=setUp.py
+
+rem checking.....
+if exist "%PYTHON_EXE%" (
+    "%PYTHON_EXE%" "%MAIN_SCRIPT%"
+) else (
+    cd venv\Scripts
+    call activate.bat
+    cd ..\..
+    python %MAIN_SCRIPT%
+)
 pause
