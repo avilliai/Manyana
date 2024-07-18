87ea94138d2db044e461d6ebb5ee65fe4cde11ad
avillia <99066610+avilliai@users.noreply.github.com>
2024-07-18 22:11:55 +0800

    更新 启动脚本.bat

--- 启动脚本.bat
+++ 启动脚本.bat
@@ -1,4 +1,16 @@
-cd venv/Scripts
-call activate.bat
-cd ../..
-python main.py
+@echo off
+set PYTHON_EXE=..\environments\Python39\python.exe
+set MAIN_SCRIPT=main.py
+
+rem checking.....
+if exist "%PYTHON_EXE%" (
+    cd venv\Scripts
+    call activate.bat
+    cd ..\..
+    "%PYTHON_EXE%" "%MAIN_SCRIPT%"
+) else (
+    echo Python doesn't exists
+    python %MAIN_SCRIPT%
+)
+
+exit /b
