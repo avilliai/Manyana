56a60faf4979dfd9a90225c01bbf5597ac4bf6ae
avillia <99066610+avilliai@users.noreply.github.com>
2024-07-18 22:21:16 +0800

    Update 启动脚本(防闪退).bat

--- 启动脚本(防闪退).bat
+++ 启动脚本(防闪退).bat
@@ -1,5 +1,14 @@
-cd venv/Scripts
-call activate.bat
-cd ../..
-python main.py
+@echo off
+set PYTHON_EXE=..\environments\Python39\python.exe
+set MAIN_SCRIPT=main.py
+
+rem checking.....
+if exist "%PYTHON_EXE%" (
+    "%PYTHON_EXE%" "%MAIN_SCRIPT%"
+) else (
+    cd venv\Scripts
+    call activate.bat
+    cd ..\..
+    python %MAIN_SCRIPT%
+)
 pause
