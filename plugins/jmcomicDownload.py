c691285e499f2fe4ff79592bbac526d3e7047022
DEII <1840094972@qq.com>
2024-07-29 19:35:16 +0800

    🥵JM相关功能权限

--- plugins/jmcomicDownload.py
+++ plugins/jmcomicDownload.py
@@ -26,21 +26,14 @@ def queryJM(name,num=4):
     client = jmcomic.JmOption.default().new_jm_client()
     page: jmcomic.JmSearchPage = client.search_site(search_query=name, page=1)
     results=[]
-    fal=0
     for i in page.content:
-        if fal>10:
-            raise Exception
-            break
-        try:
-            file = downloadComic(i[0], start=1, end=2)
-            print([f"车牌号：{i[0]} \n name：{i[1]['name']}\nauthor：{i[1]['author']}",file[0]])
-            results.append([f"车牌号：{i[0]} \n name：{i[1]['name']}\nauthor：{i[1]['author']} \n部分预览图：",file[0]])
-            if len(results) > num:
-                return results
-        except Exception as e:
-            print(e)
-            continue
-            fal+=1
+        file = downloadComic(i[0], start=1, end=2)
+        print([f"车牌号：{i[0]} \n name：{i[1]['name']}\nauthor：{i[1]['author']}",file[0]])
+        results.append([f"车牌号：{i[0]} \n name：{i[1]['name']}\nauthor：{i[1]['author']} \n部分预览图：",file[0]])
+        if len(results) > num:
+            return results
+        print(results)
+
 def downloadComic(comic_id,start=1,end=5):
     option = jmcomic.create_option_by_file('config/jmcomic.yml')
     if not os.path.exists('data/pictures/benzi'):
@@ -80,5 +73,3 @@ def downloadComic(comic_id,start=1,end=5):
 
     #png_files = [os.path.join(folder_path, file) for file in file_names if file.lower().endswith('.png')]
     return new_files
-
-#queryJM("碧蓝档案")
--- run/getcomic.py
+++ run/getcomic.py
@@ -3,7 +3,7 @@ import asyncio
 from concurrent.futures import ThreadPoolExecutor
 
 import yaml
-from mirai import GroupMessage,MessageChain,Image,FriendMessage
+from mirai import GroupMessage, MessageChain, Image, FriendMessage
 from mirai.models import ForwardMessageNode, Forward
 
 from plugins.jmcomicDownload import queryJM, downloadComic
@@ -20,7 +20,7 @@ def main(bot, logger):
     with open('data/userData.yaml', 'r', encoding='utf-8') as file:
         userdict = yaml.load(file, Loader=yaml.FullLoader)
     global superUser
-    superUser = [int(configData.get('master')),]
+    superUser = [int(configData.get('master')), ]
     for i in userdict.keys():
         data = userdict.get(i)
         times = int(str(data.get('sts')))
@@ -28,46 +28,47 @@ def main(bot, logger):
             superUser.append(str(i))
     with open('config/controller.yaml', 'r', encoding='utf-8') as f:
         controller = yaml.load(f.read(), Loader=yaml.FullLoader)
-    jmcomicSettings=controller.get("JMComic")
+    jmcomicSettings = controller.get("JMComic")
     @bot.on(GroupMessage)
     async def querycomic(event: GroupMessage):
         global superUser
         if str(event.message_chain).startswith("JM搜"):
             if jmcomicSettings.get("onlyTrustUser") and str(event.sender.id) not in superUser:
-                await bot.send(event,"用户无权限")
+                await bot.send(event, "用户无权限")
                 return
             # 分页查询，search_site就是禁漫网页上的【站内搜索】
             # 原先的执行方式将导致bot进程阻塞，任务添加到线程池，避免阻塞
-            await bot.send(event,"在找了在找了，稍等一会哦(长时间不出就是被吞了)")
+            await bot.send(event, "在找了在找了，稍等一会哦(长时间不出就是被吞了)")
             try:
                 loop = asyncio.get_running_loop()
                 # 使用线程池执行器
                 with ThreadPoolExecutor() as executor:
                     # 使用 asyncio.to_thread 调用函数并获取返回结果
-                    results=await loop.run_in_executor(executor, queryJM, querycomic)
+                    results = await loop.run_in_executor(executor, queryJM, querycomic)
             except Exception as e:
                 logger.error(e)
                 logger.exception("详细错误如下：")
             try:
-                cmList=[]
+                cmList = []
                 for i in results:
-                    cmList.append(ForwardMessageNode(sender_id=bot.qq, sender_name="ninethnine",message_chain=MessageChain([i[0],Image(path=i[1])])))
+                    cmList.append(ForwardMessageNode(sender_id=bot.qq, sender_name="ninethnine",
+                                                     message_chain=MessageChain([i[0], Image(path=i[1])])))
                 await bot.send(event, Forward(node_list=cmList))
-                await bot.send(event, "好了喵",True)
+                await bot.send(event, "好了喵", True)
             except Exception as e:
                 logger.error(e)
-                await bot.send(event, "寄了喵",True)
+                await bot.send(event, "寄了喵", True)
 
     @bot.on(GroupMessage)
     async def download(event: GroupMessage):
         if str(event.message_chain).startswith("验车"):
             if jmcomicSettings.get("onlyTrustUser") and str(event.sender.id) not in superUser:
-                await bot.send(event,"用户无权限")
+                await bot.send(event, "用户无权限")
                 return
             try:
-                comic_id = int(str(event.message_chain).replace("验车",""))
+                comic_id = int(str(event.message_chain).replace("验车", ""))
             except:
-                await bot.send(event,"无效输入 int，指令格式如下\n验车【车牌号】\n如：验车604142")
+                await bot.send(event, "无效输入 int，指令格式如下\n验车【车牌号】\n如：验车604142")
                 return
             await bot.send(event, "下载中...稍等喵")
             try:
@@ -75,16 +76,19 @@ def main(bot, logger):
                 # 使用线程池执行器
                 with ThreadPoolExecutor() as executor:
                     # 使用 asyncio.to_thread 调用函数并获取返回结果
-                    png_files=await loop.run_in_executor(executor, downloadComic, comic_id,1,jmcomicSettings.get("previewPages"))
+                    png_files = await loop.run_in_executor(executor, downloadComic, comic_id, 1,
+                                                           jmcomicSettings.get("previewPages"))
             except Exception as e:
                 logger.error(e)
-                await bot.send(event,"下载失败")
-            cmList=[]
+                await bot.send(event, "下载失败")
+            cmList = []
             logger.info(png_files)
-            cmList.append(ForwardMessageNode(sender_id=bot.qq, sender_name="ninethnine", message_chain=MessageChain("腾子吞图严重，bot仅提供本子部分页面预览。\n图片已经过处理，但不保证百分百不被吞。可能显示不出来")))
+            cmList.append(ForwardMessageNode(sender_id=bot.qq, sender_name="ninethnine", message_chain=MessageChain(
+                "腾子吞图严重，bot仅提供本子部分页面预览。\n图片已经过处理，但不保证百分百不被吞。可能显示不出来")))
             for path in png_files:
-                cmList.append(ForwardMessageNode(sender_id=bot.qq, sender_name="ninethnine",message_chain=MessageChain(Image(path=path))))
+                cmList.append(ForwardMessageNode(sender_id=bot.qq, sender_name="ninethnine",
+                                                 message_chain=MessageChain(Image(path=path))))
             await bot.send(event, Forward(node_list=cmList))
 
 
-   
+
