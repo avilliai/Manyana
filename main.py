048645d9a5763fcb36dc89c06cf8c9879a4fb833
Alpherg <99066610+avilliai@users.noreply.github.com>
2024-09-09 01:51:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ from mirai import Mirai, WebSocketAdapter, GroupMessage, Image, At, Startup, Fri
 from plugins.toolkits import newLogger,random_str,get_system_info
 from run import aiReply, voiceReply, nudgeReply, wikiHelper, imgSearch, extraParts, wReply, groupManager, \
     musicShare, LiveMonitor, aronaapi, groupGames, musicpick, scheduledTasks, appCard, aiDraw, starRail, bangumi, \
-    draftBottle, wordCloud
+    draftBottle
 
 
 # 为了实现黑名单和群开关功能，我们将继承webSocketAdapter类
@@ -374,6 +374,7 @@ if __name__ == '__main__':
         logger.error(e)
         logger.error("致命错误！定时功能无法启用，请检查设备时区")
     try:
+        from run import wordCloud
         wordCloud.main(bot, logger)
     except Exception as e:
         logger.error(e)
