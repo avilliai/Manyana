43448b78f929bfd99caee1f843d6094348bf9eaf
Alpherg <99066610+avilliai@users.noreply.github.com>
2024-09-21 20:39:37 +0800

    Update jmcomicDownload.py

--- plugins/jmcomicDownload.py
+++ plugins/jmcomicDownload.py
@@ -84,7 +84,7 @@ def downloadComic(comic_id,start=1,end=5):
         image_black_white.save(newPath)
     #png_files = [os.path.join(folder_path, file) for file in file_names if file.lower().endswith('.png')]
     return new_files
-def downloadALLAndToPdf(comic_id,savePath):
+def downloadALLAndToPdf(comic_id,savePath,URLSource=0,proxy=""):
     with open("config/jmcomic.yml", 'r', encoding='utf-8') as f: #不知道他这个options咋传的，我就修改配置文件得了。
         result = yaml.load(f.read(), Loader=yaml.FullLoader)
     tempResult = copy.deepcopy(result)
@@ -109,7 +109,9 @@ def downloadALLAndToPdf(comic_id,savePath):
     jmcomic.JmModuleConfig.CLASS_DOWNLOADER = MyDownloader
     # 使用option对象来下载本子
     jmcomic.download_album(comic_id, option)
-    r=lanzouFileToUrl(f"{savePath}/{comic_id}.pdf")
-    #r=fileToUrl(f"{savePath}/{comic_id}.pdf",proxy)
+    if URLSource==0:
+        r=lanzouFileToUrl(f"{savePath}/{comic_id}.pdf")
+    else:
+        r=fileToUrl(f"{savePath}/{comic_id}.pdf",proxy)
     return r
 
