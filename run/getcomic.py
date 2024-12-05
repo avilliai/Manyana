12bd9021d6783752a40f116a452a991c37e959ab
Alpherg <99066610+avilliai@users.noreply.github.com>
2024-12-05 16:31:00 +0800

    Update getcomic.py

--- run/getcomic.py
+++ run/getcomic.py
@@ -102,8 +102,9 @@ def main(bot, logger):
                     comic_id = int(str(event.message_chain).replace("验车", ""))
                 else:
                     context=['正在随机ing，请稍等喵~~','正在翻找好看的本子喵~','嘿嘿，JM，启动！！！！','正在翻找JM.jpg','有色色！我来了','hero来了喵~~','了解~','全力色色ing~']
-                    await bot.send(event, context[random.randint(1, len(context)) - 1])
+                    await bot.send(event, random.choice(context))
                     context=JM_search_comic_id()
+                    print(context)
                     comic_id = context[random.randint(1, len(context)) - 1]
             except:
                 await bot.send(event, "无效输入 int，指令格式如下\n验车【车牌号】\n如：验车604142",True)
