b0deadcea4ae6d40af426866f2bb936d0a6c672e
Alpherg <99066610+avilliai@users.noreply.github.com>
2024-12-10 17:42:27 +0800

    Update setUp.py

--- setUp.py
+++ setUp.py
@@ -214,7 +214,6 @@ def updaat(f=False,jump=False,source=None):
             os.system(f"\"{python_path}\" -m pip install --upgrade lanzou-api")
             os.system(f"\"{python_path}\" -m pip install --upgrade pytubefix")
             os.system(f"\"{python_path}\" -m pip install --upgrade PicImageSearch")
-            os.system(f"\"{python_path}\" -m pip install httpx==0.27.2")
     if source==None:
         logger.info("拉取bot代码\n--------------------")
         logger.info("选择更新源(git源 镜像源相互兼容)：\n1 git源\n2 git代理源1\n3 git代理源2 \n4 国内源(没事别几把用这个)")
