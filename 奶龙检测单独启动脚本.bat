de87fed15c6faa7255052ccb22cf9c520e18fc76
Alpherg <99066610+avilliai@users.noreply.github.com>
2024-12-18 13:37:17 +0800

    Create 奶龙检测单独启动脚本.bat

---
+++ 奶龙检测单独启动脚本.bat
@@ -0,0 +1,14 @@
+@echo off
+set PYTHON_EXE=..\environments\Python39\python.exe
+set MAIN_SCRIPT=nailongStraightLauncher.py
+
+rem checking.....
+if exist "%PYTHON_EXE%" (
+    "%PYTHON_EXE%" "%MAIN_SCRIPT%"
+) else (
+    cd venv\Scripts
+    call activate.bat
+    cd ..\..
+    python %MAIN_SCRIPT%
+)
+pause
